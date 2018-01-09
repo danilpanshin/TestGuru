92a3ebe089af4debd97af8423df3177eab7c5e07
danilpanshin <danilpanshin@gmail.com>
2018-01-09 18:43:44 +0300

    change correct data type to boolean

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180106155302) do
+ActiveRecord::Schema.define(version: 20180109153844) do
 
   create_table "answers", force: :cascade do |t|
     t.integer "answer", default: 0, null: false
-    t.string "correct", default: "false", null: false
+    t.boolean "correct", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
