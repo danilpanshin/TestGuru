e9050843ca8af89eadb5b1948db95c01092d0cfa
danilpanshin <danilpanshin@gmail.com>
2018-01-15 22:50:12 +0300

    change def name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  def sort_user_tests_by_level (level)
+  def user_tests_by_level (level)
     Test.joins('JOIN users_tests ON tests.id = users_tests.test_id')
          .where('tests.level = :level AND users_tests.user_id = :user_id',
                 level: level, user_id: id)
