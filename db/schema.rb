5df0c687d2eb5c760d5643274aada509837aa9e8
danilpanshin <danilpanshin@gmail.com>
2018-02-06 00:09:36 +0300

    first

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180202205309) do
+ActiveRecord::Schema.define(version: 20180205181540) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", default: "0", null: false
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20180202205309) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email"
+    t.string "password_digest"
   end
 
 end
