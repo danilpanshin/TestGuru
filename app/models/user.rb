4d0e3b853cf5edae21da0e591f3222dd241d0cf1
danilpanshin <danilpanshin@gmail.com>
2018-02-06 14:18:20 +0300

    authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,14 @@ class User < ApplicationRecord
   def test_passage(test)
   	test_passages.order(id: :desc).find_by(test_id: test.id)
   end
+
+  def authenticate(password_string)
+    digest(password_string) == self.password_digest ? self : false	
+  end
+
+  private
+
+  def digest(string)
+  	Digest::SHA1.hexdigest(string)
+  end
 end
