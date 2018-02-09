87ccb1349684f1c3be84398e9c4aabb5b5bbddf2
danilpanshin <danilpanshin@gmail.com>
2018-02-10 01:00:30 +0300

    сенсей подсказал и всё заработало

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,6 @@ class ApplicationController < ActionController::Base
   # def rescue_with_question_not_found
   #  render plain: 'Resource was not found'
   # end
-  #before_action :set_return_to_page
 
 
   helper_method :current_user,
@@ -15,25 +14,21 @@ class ApplicationController < ActionController::Base
   private
 
   def authenticate_user!
-    unless current_user
-      set_return_to_page      	
+    unless current_user 
+      cookies[:return_to] = request.fullpath  	
       redirect_to login_path, alert: 'Are you a Guru? Verify your E-mail and password please'
     end
 
   end
 
-  def current_user
+  def current_user  	
   	@current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]  	
   end
 
   def logged_in?
     current_user.present?	
   end
-
-  def set_return_to_page
-  	cookies[:return_to] ||= request.original_fullpath
-  end	
-
+  
   def user_name
     current_user.email.split('@').first.capitalize
   end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,5 @@
 class SessionsController < ApplicationController
   
-  #before_action :set_return_to_page, only: :create  
 
   def new
   end
@@ -11,7 +10,7 @@ class SessionsController < ApplicationController
     if user&.authenticate(params[:password])
       session[:user_id] = user.id 
       
-      redirect_to cookies[:return_to], notice: "Hi, #{user_name}! welcome back!"
+      redirect_to cookies[:return_to] || root_path, notice: "Hi, #{user_name}! welcome back!"
       
     else
       flash.now[:alert] = 'Are you a Guru? Verify your E-mail and password please'
