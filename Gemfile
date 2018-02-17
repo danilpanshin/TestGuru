e567676de7e8d95b340451cad19049bc5c2f9534
danilpanshin <danilpanshin@gmail.com>
2018-02-17 03:33:44 +0300

    first try

--- Gemfile
+++ Gemfile
@@ -38,6 +38,11 @@ gem 'devise', '~> 4.0'
 
 gem 'rails-i18n', '~> 5.0.0'
 
+#UI
+
+
+gem 'bootstrap', '~> 4.0.0.beta'
+
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,5 +7,14 @@ module ApplicationHelper
     link_to  project, "https://github.com/#{author}/#{repo}", target: '_blank'
   end
 
+  def flash_class(level)
+    case level
+      when 'notice' then "alert alert-info"
+      when 'success' then "alert alert-success"
+      when 'error' then "alert alert-danger"
+      when 'alert' then "alert alert-warning"
+    end
+  end
+
 
 end
