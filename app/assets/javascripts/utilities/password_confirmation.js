435c461b1f92401ef99dd51bd7f6502b7501da7d
danilpanshin <danilpanshin@gmail.com>
2018-02-28 01:04:33 +0300

    add icons

--- app/assets/javascripts/utilities/password_confirmation.js
+++ app/assets/javascripts/utilities/password_confirmation.js
@@ -2,10 +2,19 @@
 var check = function() {
   if (document.getElementById('user_password').value ==
     document.getElementById('user_password_confirmation').value)  {
+
+    document.querySelector('.octicon-smiley').classList.remove('hide')
+    document.querySelector('.octicon-alert').classList.add('hide')
+
     document.getElementById('message').style.color = 'green';
     document.getElementById('message').innerHTML = 'Пароли совпадают';
+
   } else if (document.getElementById('user_password').value !=
     document.getElementById('user_password_confirmation').value && document.getElementById('user_password_confirmation').value != '')  {
+
+    document.querySelector('.octicon-smiley').classList.add('hide')
+    document.querySelector('.octicon-alert').classList.remove('hide')
+
     document.getElementById('message').style.color = 'red';
     document.getElementById('message').innerHTML = 'Пароли не совпадают';
   }
