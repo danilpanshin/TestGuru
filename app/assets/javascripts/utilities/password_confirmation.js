b2513cbd2606456eac03fd05b7c620422bd04127
danilpanshin <danilpanshin@gmail.com>
2018-02-28 00:14:07 +0300

    пофиксил отображение сообщения password confirm

--- app/assets/javascripts/utilities/password_confirmation.js
+++ app/assets/javascripts/utilities/password_confirmation.js
@@ -4,7 +4,8 @@ var check = function() {
     document.getElementById('user_password_confirmation').value)  {
     document.getElementById('message').style.color = 'green';
     document.getElementById('message').innerHTML = 'Пароли совпадают';
-  } else {
+  } else if (document.getElementById('user_password').value !=
+    document.getElementById('user_password_confirmation').value && document.getElementById('user_password_confirmation').value != '')  {
     document.getElementById('message').style.color = 'red';
     document.getElementById('message').innerHTML = 'Пароли не совпадают';
   }
