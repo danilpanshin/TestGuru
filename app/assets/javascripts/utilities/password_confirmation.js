52eedf9d0b570068c08c4be6f4ea86cd542a7568
danilpanshin <danilpanshin@gmail.com>
2018-02-28 19:53:45 +0300

    отделил js от кода шаблонов

--- app/assets/javascripts/utilities/password_confirmation.js
+++ app/assets/javascripts/utilities/password_confirmation.js
@@ -1,22 +1,35 @@
+document.addEventListener('turbolinks:load', function () {
+    var control = document.getElementById('user_password_confirmation')
+
+    var controlPC = document.getElementById('user_password') //зачем так? для той ситуации, когда сначала вводишь пароль в поле пассворд конфирмэйшн, а потом в поле пароль. 
+
+    if (control) { control.addEventListener('input', check) }
+
+    if (controlPC) { controlPC.addEventListener('input', check) } //зачем так? для той ситуации, когда сначала вводишь пароль в поле пассворд конфирмэйшн, а потом в поле пароль. 
+});
 
 var check = function() {
-  if (document.getElementById('user_password').value ==
-    document.getElementById('user_password_confirmation').value)  {
+  var password = document.getElementById('user_password').value
+  var conf_password = document.getElementById('user_password_confirmation').value
+  var message = document.getElementById('message')
+  var smiley = document.querySelector('.octicon-smiley')
+  var alert =  document.querySelector('.octicon-alert')
 
-    document.querySelector('.octicon-smiley').classList.remove('hide')
-    document.querySelector('.octicon-alert').classList.add('hide')
+  if (password == conf_password && conf_password != '')  {
 
-    document.getElementById('message').style.color = 'green';
-    document.getElementById('message').innerHTML = 'Пароли совпадают';
+    smiley.classList.remove('hide')
+    alert.classList.add('hide')
 
-  } else if (document.getElementById('user_password').value !=
-    document.getElementById('user_password_confirmation').value && document.getElementById('user_password_confirmation').value != '')  {
+    message.style.color = 'green';
+    message.textContent = 'Пароли совпадают';
 
-    document.querySelector('.octicon-smiley').classList.add('hide')
-    document.querySelector('.octicon-alert').classList.remove('hide')
+  } else if (password != conf_password && conf_password != '')  {
 
-    document.getElementById('message').style.color = 'red';
-    document.getElementById('message').innerHTML = 'Пароли не совпадают';
+    smiley.classList.add('hide')
+    alert.classList.remove('hide')
+
+    message.style.color = 'red';
+    message.textContent = 'Пароли не совпадают';
   }
 }
 
