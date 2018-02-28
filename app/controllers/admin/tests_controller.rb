eaf5475271fff7c1009bab52de54d8e8004ace3d
danilpanshin <danilpanshin@gmail.com>
2018-03-01 01:30:25 +0300

    нашёл ошибку в js при редактировании теста

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -1,6 +1,7 @@
 class Admin::TestsController < Admin::BaseController
   
-  before_action :find_test, only: %i[show edit update destroy start] 
+  before_action :set_tests, only: %i[index update_inline]
+  before_action :find_test, only: %i[show edit update destroy start update_inline] 
   
 
   def index
@@ -39,12 +40,24 @@ class Admin::TestsController < Admin::BaseController
     end
   end
 
+  def update_inline
+    if @test.update(test_params)
+      redirect_to admin_tests_path
+    else
+      render :index
+    end
+  end
+
   def destroy
     @test.destroy
     redirect_to admin_tests_path
   end
  
   private
+
+  def set_tests
+    @tests = Test.all
+  end
 
   def test_params
     params.require(:test).permit(:title, :level, :category_id)
