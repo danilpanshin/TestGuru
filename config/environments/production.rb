a1ed77d852ceab0430c80dc5104dea3870feafd6
danilpanshin <danilpanshin@gmail.com>
2018-03-10 03:40:11 +0300

    mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,16 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "TestGuru_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'thawing-sierra-38804.herokuapp.com' }
+  config.action.mailer.delivery_method = :smtp
+  config.action.mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    user_name:            ENV['SMTP_USERNAME'],
+    password:             ENV['SMTP_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
