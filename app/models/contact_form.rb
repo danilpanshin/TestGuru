ada8efe14f4de0edde0fa927c94855aa8c5d5c75
danilpanshin <danilpanshin@gmail.com>
2018-03-10 15:38:28 +0300

    set mail form 1

--- app/models/contact_form.rb
+++ app/models/contact_form.rb
@@ -11,7 +11,7 @@ class ContactForm < MailForm::Base
   def headers
     {
       :subject => "My Contact Form",
-      :to => "Admin.first.email",
+      :to => "danilpanshin@gmail.com",
       :from => %("#{name}" <#{email}>)
     }
   end
