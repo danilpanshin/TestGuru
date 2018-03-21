74561c6219c12ed8a6f04227ab359dccc109395d
danilpanshin <danilpanshin@gmail.com>
2018-03-21 22:37:04 +0300

    update migration

--- db/migrate/20180321010008_update_foreign_key_test_passages.rb
+++ db/migrate/20180321010008_update_foreign_key_test_passages.rb
@@ -1,6 +1,6 @@
 class UpdateForeignKeyTestPassages < ActiveRecord::Migration[5.1]
   def up
-    #remove_foreign_key :test_passages, :tests
+    remove_foreign_key :test_passages, :tests
 
     add_foreign_key :test_passages, :tests, on_delete: :cascade
   end
