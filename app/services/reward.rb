ab0803b71341e80e0312de484f39a12ada059f24
danilpanshin <danilpanshin@gmail.com>
2018-03-27 02:27:07 +0300

    change award logic

--- app/services/reward.rb
+++ app/services/reward.rb
@@ -2,13 +2,15 @@ class Reward
   
   def initialize (user)
     @user = user 
+    #@badge_level = Badge.where("rule = 'level = 2'")
   end
   
   def check_rule
 
-    if @user.test_passages.last.success_test?
-      @user.badges << Badge.where("title = 'first_try'")
-      #flash[:notice] = 'Badge!!!'
+    if @user.test_passages.last.success_test? && @user.tests.where("title = ?", @user.tests.last.title).count == 1
+
+      @user.badges << Badge.where("title = 'first blood'")
+      
     end
 
   end
